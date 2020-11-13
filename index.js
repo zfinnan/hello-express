0ce8801b8797fdf32b753e98a0972eeabf310c5f
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-11-13 11:41:01 -0600

    [index] Add /about and /blog

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', function(req, res) {
     res.send('Hello, SEI World');
 });
 
+app.get('/about', function(req, res) {
+    res.send('This is the about page.');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Welcome to my blog');
+});
+
 app.listen(8000); 
